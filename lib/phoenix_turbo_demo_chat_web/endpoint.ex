a038943cc3c0067c17fc9a9b65c4db1c95b9da78
Biruk Haileye Tabor <biruk.haileye@gmail.com>
2025-05-23 16:17:48 +0300

    mix phx.gen.socket User --from-channel TurboStream

--- lib/phoenix_turbo_demo_chat_web/endpoint.ex
+++ lib/phoenix_turbo_demo_chat_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule PhoenixTurboDemoChatWeb.Endpoint do
     same_site: "Lax"
   ]
 
+  socket "/socket", PhoenixTurboDemoChatWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
